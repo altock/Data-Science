6f1f2a61088ced431032fff72f7f381e9d727189
altock <sjs382@cornell.edu>
2017-03-29 18:15:07 -0400

    Time Series for HackerRank

---
+++ TamingSpark/ratings-counter.py
@@ -0,0 +1,15 @@
+from pyspark import SparkConf, SparkContext
+import collections
+
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf = conf)
+# log4jLogger = sc._jvm.org.apache.log4j
+# log = log4jLogger.LogManager.getLogger(__name__)
+
+lines = sc.textFile("ml-100k/u.data")
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.items():
+    print("%s %i" % (key, value))
